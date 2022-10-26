a87755b185445acc1dee4ca325876efe963b8389
Amappola <anam.porrasp@gmail.com>
2022-10-26 07:08:42 -0500

    v.2.0. Job Matching #1. Kata level 8.

--- codeWars/8kyu/jobMatching1.js
+++ codeWars/8kyu/jobMatching1.js
@@ -1,16 +1,12 @@
-// Solution v.1.0.
+// Solution v.2.0.
 function match(candidate, job) {
-  // Calculate deduction from candidate's minimum salary
   let candidateMinSalary = candidate.minSalary - (candidate.minSalary * 0.1);
 
-  // Matching candidate with job
   if (typeof candidate.minSalary !== "number" || typeof job.maxSalary !== "number") {
     throw new TypeError("Missing Values");
-  } else if (candidateMinSalary <= job.maxSalary) {
-    return true
-  } else if (candidateMinSalary > job.maxSalary) {
-    return false
   }
+
+  return candidateMinSalary <= job.maxSalary
 }
 
 // Test Cases
