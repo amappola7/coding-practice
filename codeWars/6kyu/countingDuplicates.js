bd0bd5498026fdf13b750cd0a9079646b1305a93
Amappola <anam.porrasp@gmail.com>
2022-11-03 07:32:29 -0500

    New exercise

---
+++ codeWars/6kyu/countingDuplicates.js
@@ -0,0 +1,16 @@
+// Counting Duplicates
+// Link: codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/javascript
+
+// Solution v.1.0
+
+
+// Test Cases
+duplicateCount("abcde") // Expected output: 0
+duplicateCount("aabbcde") // Expected output: 2
+duplicateCount("aabBcde") // Expected output: 2
+duplicateCount("indivisibility") // Expected output: 1
+duplicateCount("Indivisibilities") // Expected output: 2
+duplicateCount("aA11") // Expected output: 2
+duplicateCount("ABBA") // Expected output: 2
+
+
