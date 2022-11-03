99a73699512747e9534a066dc24620f41a4bb5a1
Amappola <anam.porrasp@gmail.com>
2022-11-03 10:02:08 -0500

    v.#.0. Recursion powerN

---
+++ winter/recursionPowerN.js
@@ -0,0 +1,16 @@
+// Recursion Power N
+// Link: https://the-winter.github.io/codingjs/exercise.html?name=powerN&title=Recursion-1
+
+// Solution v.1.0
+function powerN(base, n){
+  if (n === 1) {
+    return base
+  } else {
+    return base * powerN(base, (n-1))
+  }
+}
+
+// Test Cases
+console.log(powerN(3, 1)) // Expected output: 3
+console.log(powerN(3, 2)) // Expected output: 9
+console.log(powerN(3, 3)) // Expected output: 27
